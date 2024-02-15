08ea10957866c70b44cfda5c326bdccc57675b9f
khosyk <khosyk@naver.com>
2024-02-15 19:14:29 +0900

    window dimension 변경 감시 훅

---
+++ src/hooks/useWindowDimensions.tsx
@@ -0,0 +1,29 @@
+import { useState, useEffect } from 'react';
+
+type WindowDimensions = {
+  width:number;
+  height:number;
+}
+
+function getWindowDimensions() {
+  const { innerWidth: width, innerHeight: height } = window;
+  return {
+    width,
+    height
+  };
+}
+
+export default function useWindowDimensions() {
+  const [windowDimensions, setWindowDimensions] = useState<WindowDimensions>(getWindowDimensions());
+
+  useEffect(() => {
+    function handleResize() {
+      setWindowDimensions(getWindowDimensions());
+    }
+
+    window.addEventListener('resize', handleResize);
+    return () => window.removeEventListener('resize', handleResize);
+  }, []);
+
+  return windowDimensions;
+}
