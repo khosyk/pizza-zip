fa5de2a86a63c68174053a445cbd1a173fc4e146
khosyk <hosy12@gmail.com>
2024-02-18 18:44:38 +0900

    sectionheader 컴포넌트화

---
+++ src/components/layout/Home/SectionHeaders.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+type Props = {
+  title:string;
+  subTitle:string;
+}
+
+export default function SectionHeaders({title,subTitle}: Props) {
+
+  return (
+    <div className="text-center">
+				<h3 className="uppercase text-gray-500 font-semibold leading-4">
+					{title}
+				</h3>
+				<h2 className="pb-3 text-primary font-bold text-4xl">{subTitle}</h2>
+		</div>
+  )
+}
