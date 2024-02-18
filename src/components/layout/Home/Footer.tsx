1daec0ee9c7133ba9580c08fa49806e490b64472
khosyk <hosy12@gmail.com>
2024-02-18 18:55:45 +0900

    footer 추가

---
+++ src/components/layout/Home/Footer.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+type Props = {};
+
+export default function Footer({}: Props) {
+	return (
+		<footer className="border-t p-8 text-center mt-16">
+			회사정보 상호 : 피자집 대표 : 이00 주소 : 서울 성동구 성덕정 0길 00, 1층
+			개인정보관리 책임자 : 이00 (leeleelee@piazzazip.com) 사업자 등록번호 :
+			211-06-76999 통신판매업 신고 : 제 성동 - 3381호
+		</footer>
+	);
+}
