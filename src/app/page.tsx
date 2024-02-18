d2594dfa0dadc6bd7c6d47006d416d8943ff2698
khosyk <hosy12@gmail.com>
2024-02-18 20:52:42 +0900

    레이아웃 변경

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,22 +1,14 @@
-import Footer from "@/components/layout/Home/Footer";
-import Header from "@/components/layout/Home/Header";
 import Hero from "@/components/layout/Home/Hero";
 import HomeAbout from "@/components/layout/Home/HomeAbout";
 import HomeMenu from "@/components/layout/Home/HomeMenu";
-import SectionHeaders from "@/components/layout/Home/SectionHeaders";
 import React from "react";
 
 export default function Home() {
-  
-  const Pizza = React.lazy(() => import('../components/layout/Home/PizzaWrap'));
-
   return (
     <>
-      <Header/>
       <Hero/>
       <HomeMenu/>
       <HomeAbout/>
-      <Footer/>
     </>
   );
 }
