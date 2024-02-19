801baa8fceaafe894744f1fb45cf138aee3fd366
khosyk <khosyk@naver.com>
2024-02-19 21:42:13 +0900

    register input action 추가

--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -1,17 +1,56 @@
+'use client'
 import Image from "next/image";
-import React from "react";
+import React, { InputHTMLAttributes, useCallback, useReducer, useState } from "react";
 
-type Props = {};
+interface State {
+  email: string;
+  password: string;
+}
 
-export default function Register({}: Props) {
+interface Action {
+  type: string;
+	payload:string;
+}
+
+export default function Register() {
+
+	const [state, dispatch] = useReducer(
+    (state:State, action:Action) => {
+      switch (action.type) {
+        case 'email':
+          return { ...state, email: action.payload};
+        case 'password':
+          return { ...state, password: action.payload};
+        default:
+          return state;
+      }
+    },
+    { email: '', password: '' }
+  );
+	
+  const handleInput = useCallback((e: React.FormEvent<HTMLInputElement>) => {
+    dispatch({ type: e.currentTarget.name, payload: e.currentTarget.value });
+  }, []);
+
+	const handleSubmit = (e:React.FormEvent<HTMLFormElement>) =>{
+		e.preventDefault();
+		fetch('/api/register',{
+			method:'POST',
+			body:JSON.stringify({
+				email:state.email,
+				password:state.password
+			}),
+			headers:{"Content-Type": 'application/json'}
+		});
+	}
+
 	return (
 		<section className="register mt-8">
 			<h1 className="text-center text-primary text-4xl">Register</h1>
-			<form className="block max-w-xs mx-auto mt-5" action="">
-				<input type="text" placeholder="email" />
-				<input type="password" placeholder="password" />
-				<input type="password" placeholder="password" />
-				<button type="submit">등록</button>
+			<form onSubmit={handleSubmit} className="block max-w-xs mx-auto mt-5" action="">
+				<input type="text" name='email' placeholder="email" onChange={handleInput}  value={state.email}/>
+				<input type="password" name='password' placeholder="password" onChange={handleInput} value={state.password} />
+			<button type="submit">등록</button>
 				<div className="my-4 text-center text-gray-500">
 					또는 아래 방법으로 로그인
 				</div>
