2c7678aac40ce6a65fa8725ed2cca05363a95512
khosyk <hosy12@gmail.com>
2024-02-23 19:29:55 +0900

    등록 에러 수정, 로그인 페이지 링크 추가

--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -1,73 +1,121 @@
-'use client'
+"use client";
 import Image from "next/image";
-import React, { InputHTMLAttributes, Reducer, ReducerState, useCallback, useReducer, useState } from "react";
+import Link from "next/link";
+import React, { useCallback, useReducer, useState } from "react";
 
 interface State {
-  email: string;
-  password: string;
+	email: string;
+	password: string;
 }
 
 interface Action {
-  type: string;
-	payload:string;
+	type: string;
+	payload: string;
 }
 
 export default function Register() {
-
+	const [creatingUser, setCreatingUser] = useState(false);
+	const [userCreated, setUserCreated] = useState(false);
+	const [duplicate, setDuplicate] = useState(false);
 	const [state, dispatch] = useReducer(
-    (state:State, action:Action) => {
-      switch (action.type) {
-        case 'email':
-          return { ...state, email: action.payload};
-        case 'password':
-          return { ...state, password: action.payload};
-        default:
-          return state;
-      }
-    },
-    { email: '', password: '' }
-  );
-	
-  const handleInput = useCallback((e: React.FormEvent<HTMLInputElement>) => {
-    dispatch({ type: e.currentTarget.name, payload: e.currentTarget.value });
-  }, []);
+		(state: State, action: Action) => {
+			switch (action.type) {
+				case "email":
+					return { ...state, email: action.payload };
+				case "password":
+					return { ...state, password: action.payload };
+				default:
+					return state;
+			}
+		},
+		{ email: "", password: "" }
+	);
 
-	const handleSubmit = async (e:React.FormEvent<HTMLFormElement>) =>{
+	const handleInput = useCallback((e: React.FormEvent<HTMLInputElement>) => {
+		dispatch({ type: e.currentTarget.name, payload: e.currentTarget.value });
+	}, []);
+
+	const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
 		e.preventDefault();
-		try{
-			const res = await fetch('/api/register',{
-			method:'POST',
-			body:JSON.stringify({
-				email:state.email,
-				password:state.password
-			}),
-			headers:{"Content-Type": 'application/json'}
-		})
-		if(!res.ok){
-			console.log(res.statusText);
-			if(res.statusText.includes('EMAIL')) throw Error("email");
-			if(res.statusText.includes('PASSWORD')) throw Error("password");
+		try {
+			setCreatingUser(true);
+			setUserCreated(false);
+			setDuplicate(false);
+			const res = await fetch("/api/register", {
+				method: "POST",
+				body: JSON.stringify({
+					email: state.email,
+					password: state.password,
+				}),
+				headers: { "Content-Type": "application/json" },
+			});
+			if (!res.ok) {
+				if(res.statusText.includes('duplicate')) throw Error('duplicate');
+				throw Error();
+			}
+			setCreatingUser(false);
+			setUserCreated(true);
+		} catch (err) {
+			setCreatingUser(false);
+			if (err instanceof Error) {
+				setDuplicate(true);
+			}
 		}
-		console.log("SUCCEDD::", await res.json());
-	}catch(err){
-			console.log("ISEROR:",err);
-		}
-	}
+	};
 
 	return (
 		<section className="register mt-8">
-			<h1 className="text-center text-primary text-4xl">Register</h1>
-			<form onSubmit={handleSubmit} className="block max-w-xs mx-auto mt-5" action="">
-				<input type="text" name='email' placeholder="email" onChange={handleInput}  value={state.email}/>
-				<input type="password" name='password' placeholder="password" onChange={handleInput} value={state.password} />
-			<button type="submit">등록</button>
+			<h1 className="text-center font-semibold text-primary text-4xl">회원가입</h1>
+			<form
+				onSubmit={handleSubmit}
+				className="block max-w-xs mx-auto mt-5"
+				action="">
+				<input
+					type="text"
+					name="email"
+					placeholder="email"
+					onChange={handleInput}
+					value={state.email}
+					disabled={creatingUser}
+				/>
+				<input
+					type="password"
+					name="password"
+					placeholder="password"
+					onChange={handleInput}
+					value={state.password}
+					disabled={creatingUser}
+				/>
+				<button type="submit" disabled={creatingUser}>
+					회원가입
+				</button>
+				{userCreated && (
+				<div className="my-4 text-center">
+					회원가입 완료!
+					<br/>
+					<Link className='hover:underline' href={'/login'}>
+						로그인하기 &raquo;
+					</Link>
+				</div>
+			)}
+			{duplicate && (
+				<div className="my-4 text-center text-primary">
+					이미 가입된 계정입니다.
+				</div>
+			)}
 				<div className="my-4 text-center text-gray-500">
 					또는 아래 방법으로 로그인
 				</div>
 				<button className="flex justify-center gap-4">
-          <Image src={"/google.png"} alt="googleLogo" width={24} height={24} />
+					<Image src={"/google.png"} alt="googleLogo" width={24} height={24} />
 					구글 로그인
 				</button>
+				<div className="my-6 text-center pt-5  border-t text-gray-500">
+					이미 회원이신가요?
+					<Link className='pl-1 hover:underline' href={'/login'}>
+						로그인하기 &raquo;
+					</Link>
+				</div>
 			</form>
 		</section>
 	);
