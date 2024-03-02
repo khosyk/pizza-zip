bb3b6f6de20e1fdc29710aef9d20e45bdeb59d25
khosyk <hosy12@gmail.com>
2024-03-02 21:23:55 +0900

    user schema name added

--- src/app/api/models/User.ts
+++ src/app/api/models/User.ts
@@ -2,22 +2,25 @@ import { Schema, models, model } from 'mongoose'
 import bcrypt from 'bcrypt'
 
 type User = {
+  name:string
   email: string
   password: string
 }
 
 const emailRegex = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/
+//8~20자 영문자or수자or특수문자 2가지 이상 조합
 const passwordRegex =
-  /^(?!((?:[A-Za-z]+)|(?:[~!@#$%^&*()_+=]+)|(?:[0-9]+))$)[A-Za-z\d~!@#$%^&*()_+=]{8,20}$/ //8~20자 영문자or수자or특수문자 2가지 이상 조합
+  /^(?!((?:[A-Za-z]+)|(?:[~!@#$%^&*()_+=]+)|(?:[0-9]+))$)[A-Za-z\d~!@#$%^&*()_+=]{8,20}$/ 
 
 const UserSchema = new Schema<User>(
   {
+    name: { type: String },
     email: {
       type: String,
       required: true,
       unique: true,
       validate: {
-        validator: function (email: string) {
+        validator: (email: string)=> {
           if (!emailRegex.test(email)) {
             throw new Error('EMAIL')
           }
@@ -28,8 +31,7 @@ const UserSchema = new Schema<User>(
       type: String,
       required: true,
       validate: {
-        validator: function (pass: string) {
-          //커스텀 벨리데이터 작성
+        validator: (pass: string) =>{
           if (!(pass.length > 8 && pass.length < 21))
             throw new Error('PASSWORD')
           if (!passwordRegex.test(pass)) throw new Error('PASSWORD')
