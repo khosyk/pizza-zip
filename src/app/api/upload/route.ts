a2e631f578afecc8af0c62f7afddf8c271a7ab1d
khosyk <hosy12@gmail.com>
2024-03-07 21:53:38 +0900

    image edit updated

--- src/app/api/upload/route.ts
+++ src/app/api/upload/route.ts
@@ -1,7 +1,7 @@
 /* eslint-disable import/prefer-default-export */
-import uniqId from 'uniqid'
 import { NextRequest } from 'next/server'
 import { PutObjectCommand, S3Client } from '@aws-sdk/client-s3'
+import uniqid from 'uniqid';
 
 export async function POST(req: NextRequest) {
   const data = await req.formData()
@@ -9,26 +9,29 @@ export async function POST(req: NextRequest) {
   if (data.get('file')) {
     const file = data?.get('file')
 
-    const s3Client = new S3Client([{
-      region: 'us-east-1',
-      credentials: {
-        accessKeyId: process.env.AWS_ACCESS_KEY,
-        secretAccessKey: process.env.AWS_SECRET_KEY
-      },
-    }])
+    const s3Client = new S3Client({
+      region: 'ap-northeast-2',
+      credentials:{
+      accessKeyId: process.env.AWS_ACCESS_KEY,
+      secretAccessKey: process.env.AWS_SECRET_KEY
+      }
+    })
 
-    const ext = file?.name.split('.').slice(-1)
+    let ext:string = '.png'
+    if (file instanceof File) {
+      ext = file?.name.split('.').slice(-1)[0]
 
-    const newFileName = uniqId() + '.' + ext
+    // eslint-disable-next-line prefer-template
+    const newFileName = uniqid() + '.' + ext
 
     const chunks: File[] = []
-
-    for await (const chunk of file?.stream()) {
-      chunks.push(chunks)
+    for await(let chunk of file.stream()) {
+      chunks.push(chunk)
     }
+
     const buffer = Buffer.concat(chunks);
 
-    const bucket = 'pizzzip'
+    const bucket = 'pizzazip'
     await s3Client.send(new PutObjectCommand({
       Bucket: bucket,
       Key: newFileName,
@@ -37,8 +40,10 @@ export async function POST(req: NextRequest) {
       Body: buffer
     }))
 
-    const link = `https://${bucket}.s3.amazonaws.com/${newFileName}`
+    const link = `https://pizzazip.s3.amazonaws.com/${newFileName}`
     return Response.json(link)
+    
+    }
   }
-
+  return Response.json(true)
 }
