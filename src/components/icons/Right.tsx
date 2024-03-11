7718e377a5c0c9337ee9f272df34f829e32d98bd
khosyk <hosy12@gmail.com>
2024-03-11 21:53:10 +0900

    added defaultProps

--- src/components/icons/Right.tsx
+++ src/components/icons/Right.tsx
@@ -1,8 +1,13 @@
 import React from 'react'
 
+const defaultProps ={
+  className:'w-6 h-6'
+}
+
 type Props = {
   className?: string
 }
+
 
 export default function Right({ className = 'w-6 h-6' }: Props) {
   return (
@@ -10,15 +15,17 @@ export default function Right({ className = 'w-6 h-6' }: Props) {
       xmlns="http://www.w3.org/2000/svg"
       fill="none"
       viewBox="0 0 24 24"
-      stroke-width="1.5"
+      strokeWidth="1.5"
       stroke="currentColor"
       className={className}
     >
       <path
-        stroke-linecap="round"
-        stroke-linejoin="round"
+        strokeLinecap="round"
+        strokeLinejoin="round"
         d="m12.75 15 3-3m0 0-3-3m3 3h-7.5M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"
       />
     </svg>
   )
 }
+
+Right.defaultProps = defaultProps
